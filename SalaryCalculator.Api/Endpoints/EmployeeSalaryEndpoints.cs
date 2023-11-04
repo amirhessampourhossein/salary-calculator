0c458b2514ea1d275a27af3b8fac981080c8ef2b
Amir Hesam <Amir Hesam@HESAM>
2023-11-04 09:31:41 +0330

    move endpoints to a seperate file

---
+++ SalaryCalculator.Api/Endpoints/EmployeeSalaryEndpoints.cs
@@ -0,0 +1,79 @@
+﻿using MediatR;
+using SalaryCalculator.Api.Requests;
+using SalaryCalculator.Application.EmployeeSalaries.CreateEmployeeSalary;
+using SalaryCalculator.Application.EmployeeSalaries.DeleteEmployeeSalary;
+using SalaryCalculator.Application.EmployeeSalaries.GetEmployeeSalary;
+using SalaryCalculator.Application.EmployeeSalaries.GetRangeEmployeeSalaries;
+using SalaryCalculator.Application.EmployeeSalaries.UpdateEmployeeSalary;
+
+namespace SalaryCalculator.Api.Endpoints;
+
+public static class EmployeeSalaryEndpoints
+{
+    public static void MapEmployeeSalaryEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/salary");
+
+        group.MapPost("/{datatype}", CreateEmployeeSalary)
+            .WithName(nameof(CreateEmployeeSalary))
+            .WithOpenApi();
+
+        group.MapPut("/{datatype}/{id}", UpdateEmployeeSalary)
+            .WithName(nameof(UpdateEmployeeSalary))
+            .WithOpenApi();
+
+        group.MapDelete("/{id}", DeleteEmployeeSalary)
+            .WithName(nameof(DeleteEmployeeSalary))
+            .WithOpenApi();
+
+        group.MapGet("/{id}", GetEmployeeSalary)
+            .WithName(nameof(GetEmployeeSalary))
+            .WithOpenApi();
+
+        group.MapGet("/", GetEmployeeSalaries)
+            .WithName(nameof(GetEmployeeSalaries))
+            .WithOpenApi();
+    }
+
+    public static async Task<IResult> CreateEmployeeSalary(ISender sender, string dataType, EmployeeSalaryOvertimeRequestBody request)
+    {
+        var result = await sender.Send(new CreateEmployeeSalaryCommand(
+                request.Data,
+                dataType,
+                request.OvertimeCalculator));
+
+        return TypedResults.Json(result, statusCode: (int)result.StatusCode);
+    }
+
+    public static async Task<IResult> UpdateEmployeeSalary(ISender sender, string dataType, Guid id, EmployeeSalaryOvertimeRequestBody request)
+    {
+        var result = await sender.Send(new UpdateEmployeeSalaryCommand(
+                id,
+                request.Data,
+                dataType,
+                request.OvertimeCalculator));
+
+        return TypedResults.Json(result, statusCode: (int)result.StatusCode);
+    }
+
+    public static async Task<IResult> DeleteEmployeeSalary(ISender sender, Guid id)
+    {
+        var result = await sender.Send(new DeleteEmployeeSalaryCommand(id));
+
+        return TypedResults.Json(result, statusCode: (int)result.StatusCode);
+    }
+
+    public static async Task<IResult> GetEmployeeSalary(ISender sender, Guid id)
+    {
+        var result = await sender.Send(new GetEmployeeSalaryQuery(id));
+
+        return TypedResults.Json(result, statusCode: (int)result.StatusCode);
+    }
+
+    public static async Task<IResult> GetEmployeeSalaries(ISender sender, string startDate, string endDate)
+    {
+        var result = await sender.Send(new GetRangeEmployeeSalaryQuery(startDate, endDate));
+
+        return TypedResults.Json(result, statusCode: (int)result.StatusCode);
+    }
+}
--- SalaryCalculator.Api/Program.cs
+++ SalaryCalculator.Api/Program.cs
@@ -1,12 +1,6 @@
-using MediatR;
+using SalaryCalculator.Api.Endpoints;
 using SalaryCalculator.Api.Middlewares;
-using SalaryCalculator.Api.Requests;
 using SalaryCalculator.Application;
-using SalaryCalculator.Application.EmployeeSalaries.CreateEmployeeSalary;
-using SalaryCalculator.Application.EmployeeSalaries.DeleteEmployeeSalary;
-using SalaryCalculator.Application.EmployeeSalaries.GetEmployeeSalary;
-using SalaryCalculator.Application.EmployeeSalaries.GetRangeEmployeeSalaries;
-using SalaryCalculator.Application.EmployeeSalaries.UpdateEmployeeSalary;
 using SalaryCalculator.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,48 +25,6 @@ app.UseHttpsRedirection();
 
 app.UseExceptionHandlingMiddleware();
 
-var group = app.MapGroup("/api/salary");
-
-group.MapPost("/{datatype}", async (ISender sender, string dataType, EmployeeSalaryOvertimeRequestBody request) =>
-{
-    var result = await sender.Send(new CreateEmployeeSalaryCommand(
-        request.Data,
-        dataType,
-        request.OvertimeCalculator));
-
-    return TypedResults.Json(result, statusCode: (int)result.StatusCode);
-});
-
-group.MapPut("/{dataType}/{id}", async (ISender sender, string dataType, Guid id, EmployeeSalaryOvertimeRequestBody request) =>
-{
-    var result = await sender.Send(new UpdateEmployeeSalaryCommand(
-        id,
-        request.Data,
-        dataType,
-        request.OvertimeCalculator));
-
-    return TypedResults.Json(result, statusCode: (int)result.StatusCode);
-});
-
-group.MapDelete("/{id}", async (ISender sender, Guid id) =>
-{
-    var result = await sender.Send(new DeleteEmployeeSalaryCommand(id));
-
-    return TypedResults.Json(result, statusCode: (int)result.StatusCode);
-});
-
-group.MapGet("/{id}", async (ISender sender, Guid id) =>
-{
-    var result = await sender.Send(new GetEmployeeSalaryQuery(id));
-
-    return TypedResults.Json(result, statusCode: (int)result.StatusCode);
-});
-
-group.MapGet("/", async (ISender sender, string StartDate, string EndDate) =>
-{
-    var result = await sender.Send(new GetRangeEmployeeSalaryQuery(StartDate, EndDate));
-
-    return TypedResults.Json(result, statusCode: (int)result.StatusCode);
-});
+app.MapEmployeeSalaryEndpoints();
 
 app.Run();
