37d1ad7b9e92d849b2105379437d0dcd4bb2f58f
Amir Hesam <Amir Hesam@HESAM>
2024-01-20 18:31:11 +0330

    Update to .net8

--- src/SalaryCalculator.Application/Abstractions/IStringMapper.cs
+++ src/SalaryCalculator.Application/Abstractions/IStringMapper.cs
@@ -1,6 +1,6 @@
 ﻿namespace SalaryCalculator.Application.Abstractions;
 
-public interface IStringMapper<TDestination> 
+public interface IStringMapper<TDestination>
     where TDestination : class
 {
     TDestination Map(string data, string dataType);
--- src/SalaryCalculator.Domain/EmployeeSalaries/Date.cs
+++ src/SalaryCalculator.Domain/EmployeeSalaries/Date.cs
@@ -4,7 +4,7 @@ namespace SalaryCalculator.Domain.EmployeeSalaries;
 
 public record Date(DateOnly Value)
 {
-    public string ToInvariantDate() 
+    public string ToInvariantDate()
         => Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
 
     public static implicit operator Date(DateOnly Value) => new(Value);
